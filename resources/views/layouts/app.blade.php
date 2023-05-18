2d1eb5c156298ec33d9ecdabcf7a088f0cae4e74
rachmatfajar <rachmat3@mhs.unsyiah.ac.id>
2023-05-18 13:36:38 +0700

    fix: update file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,11 +20,11 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div id="app" style="background-image: url(bg_login.jpg)" class="bg-cover bg-center overflow-y-auto overflow-x-hidden flex-1">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Book Me') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,34 +1,32 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav  sidebar bg-indigo-900 accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
-                <div class="sidebar-brand-text mx-3">{{ __('Homepage') }}</div>
+            <a class="sidebar-brand d-flex align-items-center justify-content-center">
+                <div class="sidebar-brand-text mx-3 text-white">{{ __('Book Me') }}</div>
+                <img src="{{ asset('/logo_infor.png') }}" alt="Nama Gambar" class="w-1/2">
             </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+            
 
             <li class="nav-item {{ request()->is('admin/dashboard') || request()->is('admin/dashboard') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('admin.dashboard.index') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>{{ __('Dashboard') }}</span></a>
+                    <span class="text-white">{{ __('Dashboard') }}</span></a>
             </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
             
             
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('admin.bookings.index') }}">
                     <i class="fas fa-fw fa-calendar"></i>
-                    <span>{{ __('Booking') }}</span></a>
+                    <span class="text-white">{{ __('Booking') }}</span></a>
             </li>
 
                      <!-- Nav Item  -->
              <li class="nav-item {{ request()->is('admin/system_calendars') || request()->is('admin/system_calendars') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('admin.system_calendars.index') }}">
                     <i class="fas fa-fw fa-calendar"></i>
-                    <span>{{ __('Calendar') }}</span></a>
+                    <span class="text-white">{{ __('Calendar') }}</span></a>
             </li>
 
 
